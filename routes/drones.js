bfda61f976bc10b46179f79d925c46dce1426d69
lluisllc <lluis.llacera@gmail.com>
2022-06-27 18:02:59 +0200

    Iteración 4 incluida
    Iteración 4 incluida

--- routes/drones.js
+++ routes/drones.js
@@ -27,14 +27,32 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  console.log(id)
+
+  Drone.findById(id)
+    .then(droneToEdit => {
+
+      console.log("hola", droneToEdit);
+      res.render('drones/update-form.hbs', droneToEdit);
+    })
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  console.log(req.body);
+  const { name, propellers, maxSpeed } = req.body;
+  const { Id } = req.params;
+
+  Drone.findByIdAndUpdate(Id, { name, propellers, maxSpeed }, { new: true })
+    .then(updatedDrones => res.redirect(`/drones/${updatedDrones.id}`))
+    .catch(error => next(error));
 });
+
+// Model.findByIdAndUpdate(req.params.theId, req.body, { new: true })
+// .then(res.redirect(`/drones/${updatedDrones.id}`))
+// .catch(error => next(error));
+
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
