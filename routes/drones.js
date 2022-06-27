631151ec77e7d4d30a5809e017436c055a2b4710
lluisllc <lluis.llacera@gmail.com>
2022-06-27 13:26:39 +0200

    Casi 2
    iteración 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const Drone = require('./../models/Drone.model');
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(response => {
+      console.log("hola",response);
+      res.render("./../views/drones/list.hbs", { response });
+    })
+    .catch(error => console.log(error));
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  // .then((res) => {
+  //   return Drone.create({
+  //     name: 'Halcon Milenario',
+  //     propellers: 111,
+  //     maxSpeed:420,
+  //   })
+  // })
+  // .catch((err) => console.log(err))
 });
 
 router.post('/drones/create', (req, res, next) => {
